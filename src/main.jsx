695b01e02d2e01fbf5524cf9018a1ac7accdbd3e
Pulkit Jain <pulkitjainhk@gmail.com>
2024-11-14 23:42:47 +0530

    Refactor createRoot method in main.jsx to fix rendering issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { Toaster } from './components/ui/sonner.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID} >
       <App />
